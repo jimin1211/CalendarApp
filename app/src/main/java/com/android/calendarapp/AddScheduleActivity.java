469541ebd973b2daf35345b0e6ec0ace4a33f015
jimin1211 <rlawlalsy@naver.com>
2021-06-12 10:41:26 +0900

    상세일정 액티비티

--- app/src/main/java/com/android/calendarapp/AddScheduleActivity.java
+++ app/src/main/java/com/android/calendarapp/AddScheduleActivity.java
@@ -1,21 +1,48 @@
 package com.android.calendarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.core.app.ActivityCompat;
 
-import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
 import java.text.DateFormat;
+import java.util.List;
+import java.util.Locale;
 
-public class AddScheduleActivity extends AppCompatActivity {
+public class AddScheduleActivity extends AppCompatActivity implements OnMapReadyCallback {
     private String year, month, day, time;
+    private FusedLocationProviderClient mFusedLocationClient;
+    GoogleMap mGoogleMap = null;
+    final private String TAG = "LocationService";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +55,7 @@ public class AddScheduleActivity extends AppCompatActivity {
         time = intent.getExtras().getString("time");
 
         EditText title = (EditText)findViewById(R.id.title);
+        title.setPrivateImeOptions("defaultInputmode=korean;"); //한글자판 입력
         title.setText(year+"년 "+month+"월 "+day+"일 "+time+"시");
 
         TimePicker sTimePicker = (TimePicker)findViewById(R.id.start_time_picker);
@@ -38,42 +66,117 @@ public class AddScheduleActivity extends AppCompatActivity {
         eTimePicker.setCurrentHour(Integer.parseInt(time)+1);
         eTimePicker.setCurrentMinute(0);
 
-        /*TimePickerDialog mTimePickerDialog = new TimePickerDialog(AddScheduleActivity.this, new TimePickerDialog.OnTimeSetListener() {
+        EditText mapinput = (EditText)findViewById(R.id.mapinput);
+        mapinput.setPrivateImeOptions("defaultInputmode=korean;");
+        Button search = (Button)findViewById(R.id.search);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLastLocation(); //Activity가 실행되자 마자 이 함수가 실행 됨
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this); //XML레이아웃에 있는 mapFragment객체를 얻어와서 메소드 호출.
+
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            public void onClick(View view) {
+                if (mGoogleMap != null) {
+                    Geocoder geocoder = new Geocoder(AddScheduleActivity.this, Locale.KOREA);
+                    Address addr;
+
+                    try {
+                        List<Address> listAddress = geocoder.getFromLocationName(mapinput.getText().toString(), 1);
+                        if (listAddress.size() > 0) { // 주소값이 존재 하면
+                            addr = listAddress.get(0); // Address형태로
+                            /*lat = (int) (addr.getLatitude());
+                            lng = (int) (addr.getLongitude());*/
+
+                            //Log.d(TAG, "주소로부터 취득한 위도 : " + lat + ", 경도 : " + lng);
+
+                            LatLng location = new LatLng(addr.getLatitude(), addr.getLongitude());
+
+                            mGoogleMap.addMarker(
+                                    new MarkerOptions().
+                                            position(location).
+                                            title(mapinput.getText().toString()));
+
+                            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
             }
-        },Integer.parseInt(time),0,true );*/
+        });
 
+        EditText memo = (EditText)findViewById(R.id.memo);
+        memo.setPrivateImeOptions("defaultInputmode=korean;"); //한글자판 입력
 
+        Button save = (Button)findViewById(R.id.save);
 
-        /*Fragment fragment = new AddScheduleFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString("year", year);
-        bundle.putString("month", month);
-        bundle.putString("day", day);
-        bundle.putString("time", time);
-        fragment.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction().add(R.id.add_schedule_container, fragment).commit();*/
-
-/*
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        //MainActivity가 AppCompatActivity를 확장하였으므로, FragmentManager 인스턴스 얻을 때 getSupportFragmentManager() 사용
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        //fragment 추가, 삭제 또는 교체 등의 작업 수행 중 오류 발생 시, 다시 원래 상태로 되돌릴 수 있도록, FragmentTransaction 클래스 이용
-        //FragmentManager의 beginTransaction() 메소드 호출 통해 FragmentTransaction의 인스턴스 얻어옴
-        fragmentTransaction.add(R.id.add_schedule_container, new AddScheduleFragment());
-        //FragmentTransaction의 add() 메소드 통해 동적으로 fragment 추가
-        fragmentTransaction.commit();
-        //fragmentTransaction의 변경사항 저장*/
-    }
-    public Dialog onCreateDialog (Bundle savedInstanceState){
-        TimePickerDialog mTimePickerDialog = new TimePickerDialog(AddScheduleActivity.this, new TimePickerDialog.OnTimeSetListener() {
+        Button cancle = (Button)findViewById(R.id.cancel);
+        cancle.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                hourOfDay = Integer.parseInt(time);
-                minute = 0;
+            public void onClick(View view) {
+                onBackPressed(); //뒤로가기
             }
-        },Integer.parseInt(time),0,true );
-        return mTimePickerDialog;
+        });
+
+        Button remove = (Button)findViewById(R.id.remove);
     }
+
+
+
+    final int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 0;
+    Location mLastLocation;
+
+    private void getLastLocation() {
+        // 1. 위치 접근에 필요한 권한 검사 및 요청
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    AddScheduleActivity.this,            // MainActivity 액티비티의 객체 인스턴스를 나타냄
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},        // 요청할 권한 목록을 설정한 String 배열
+                    REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION    // 사용자 정의 int 상수. 권한 요청 결과를 받을 때
+            );
+            return; //return으로 하지 말고 onRequestPermissionsResult으로 하기
+        }
+
+        // 2. Task<Location> 객체 반환
+        //mFusedLocationClient : 위치정보 제공자
+        Task task = mFusedLocationClient.getLastLocation(); //위치 정보를 반환받기 위해(마지막으로 알려진 위치!!)
+
+        // 3. Task가 성공적으로 완료 후 호출되는 OnSuccessListener 등록
+        task.addOnSuccessListener(this, new OnSuccessListener<Location>() { //위치 정보를 획득하게 되면 불려짐
+            @Override
+            public void onSuccess(Location location) { //위치정보를 얻으면 onSuccess 메소드가 불려짐
+
+                // 4. 마지막으로 알려진 위치(location 객체)를 얻음.
+                if (location != null) {
+                    mLastLocation = location;
+                    //updateUI(); //이건 필요 없음!!
+                } else
+                    Toast.makeText(getApplicationContext(),
+                            "No location detected",
+                            Toast.LENGTH_SHORT)
+                            .show();
+            }
+        });
+    }
+
+    public void onMapReady(final GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        LatLng hansung = new LatLng(37.5817891, 127.009854);
+        mGoogleMap.addMarker(
+                new MarkerOptions().
+                        position(hansung).
+                        title("한성대학교")); //초기값
+
+        // move the camera
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hansung, 15));
+    }
+
 }
